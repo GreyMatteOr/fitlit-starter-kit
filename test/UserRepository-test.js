af3de967631319767f17b4c075e46f9af6df4b03
Eric Berglund <ericberglund117@gmail.com>
2020-08-21 19:18:16 -0600

    Intialize UserRepository test

--- test/UserRepository-test.js
+++ test/UserRepository-test.js
@@ -1,4 +1,22 @@
 const chai = require("chai");
 const expect = chai.expect;
 
-const User = require('../src/User');
+const UserRepository = require('../src/UserRepository');
+
+describe('UserRepository', function() {
+
+  let userRepository;
+  beforeEach(function() {
+    userRepository = new UserRepository();
+  });
+
+  describe('Intialization', function() {
+
+    it('should be a function', function() {
+      expect(UserRepository).to.be.a('function');
+    });
+    it('should be an instance of UserRepository', function() {
+      expect(userRepository).to.be.an.instanceof(UserRepository);
+    });
+  });
+});
