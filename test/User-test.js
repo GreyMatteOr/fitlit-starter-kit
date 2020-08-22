cf15b4e85b42736a32acdae90035f2935ef76e02
Eric Berglund <ericberglund117@gmail.com>
2020-08-21 19:25:09 -0600

    Initialize User test

--- test/User-test.js
+++ test/User-test.js
@@ -2,3 +2,21 @@ const chai = require("chai");
 const expect = chai.expect;
 
 const User = require('../src/User');
+
+describe('User', function() {
+
+  let user;
+  beforeEach(function() {
+    user = new User();
+  });
+
+  describe('Intialization', function() {
+
+    it('should be a function', function() {
+      expect(User).to.be.a('function');
+    });
+    it('should be an instance of User', function() {
+      expect(user).to.be.an.instanceof(User);
+    });
+  });
+});
