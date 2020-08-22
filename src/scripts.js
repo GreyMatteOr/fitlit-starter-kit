459f680246044d1d6131d055adfd0272f9df8294
Eric Berglund <ericberglund117@gmail.com>
2020-08-22 10:59:05 -0600

    Create average step goal display

--- src/scripts.js
+++ src/scripts.js
@@ -1,14 +1,17 @@
 let greeting = document.querySelector('h1');
+const users = new UserRepository(userData);
 window.onload = onLoad
 
 function onLoad () {
   let randomUser = getRandomUser();
-  greeting.innerText = `Hello ${randomUser.getFirstName()}!`
+  let userDailyStepGoal = randomUser.dailyStepGoal
+  let averageUsersStepGoal = users.calculateAverageStepGoal();
+  greeting.innerText = `Hello ${randomUser.getFirstName()}! Your step goal of ${userDailyStepGoal} is ${(userDailyStepGoal > averageUsersStepGoal) ? 'more than' : 'close to'} the average step goal among users of ${averageUsersStepGoal}.`
 }
 
 function getRandomUser() {
-  let randomIndex = Math.floor(Math.random() * userData.length)
-  return new User(userData[randomIndex]);
+  let randomIndex = Math.floor(Math.random() * users.data.length)
+  return new User(users.data[randomIndex]);
 }
 
 function getUserInfo() {
