af0043664b3aa80aa3658d709d1bd070c96e5b49
Eric Berglund <ericberglund117@gmail.com>
2020-08-21 19:20:30 -0600

    Initialize Sleep test

--- test/Sleep-test.js
+++ test/Sleep-test.js
@@ -1,4 +1,22 @@
 const chai = require("chai");
 const expect = chai.expect;
 
-const User = require('../src/User');
+const Sleep = require('../src/Sleep');
+
+describe('Sleep', function() {
+
+  let sleep;
+  beforeEach(function() {
+    sleep = new Sleep();
+  });
+
+  describe('Intialization', function() {
+
+    it('should be a function', function() {
+      expect(Sleep).to.be.a('function');
+    });
+    it('should be an instance of Sleep', function() {
+      expect(sleep).to.be.an.instanceof(Sleep);
+    });
+  });
+});
