751df11bfe4bde84d4c74efddd4df4d17844778c
Eric Berglund <ericberglund117@gmail.com>
2020-08-22 10:47:33 -0600

    Initialize script.js to display random user first name

--- src/scripts.js
+++ src/scripts.js
@@ -1,1 +1,16 @@
-console.log("Hello World");
+let greeting = document.querySelector('h1');
+window.onload = onLoad
+
+function onLoad () {
+  let randomUser = getRandomUser();
+  greeting.innerText = `Hello ${randomUser.getFirstName()}!`
+}
+
+function getRandomUser() {
+  let randomIndex = Math.floor(Math.random() * userData.length)
+  return new User(userData[randomIndex]);
+}
+
+function getUserInfo() {
+
+}
