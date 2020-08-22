7c690cb67e8bf6b8a7d9d0ec7daf8d10a4978fca
Matthew Lane <themplane@gmail.com>
2020-08-21 18:51:31 -0700

    Add initialization Tests for UserRepository

---
+++ src/UserRepository.js
@@ -0,0 +1,9 @@
+class UserRepository{
+  constructor(hydration = {}, sleep = {}, activity ={}) {
+    this.hydration = hydration;
+    this.sleep = sleep;
+    this.activity = activity;
+  }
+}
+
+module.exports = UserRepository;
--- test/UserRepository-test.js
+++ test/UserRepository-test.js
@@ -15,8 +15,22 @@ describe('UserRepository', function() {
     it('should be a function', function() {
       expect(UserRepository).to.be.a('function');
     });
+
     it('should be an instance of UserRepository', function() {
       expect(userRepository).to.be.an.instanceof(UserRepository);
+    });
+
+    it('should have a hydration property', function() {
+      expect(userRepository).to.have.property('hydration');
+    });
+
+
+    it('should have a hydration property', function() {
+      expect(userRepository).to.have.property('sleep');
+    });
+
+    it('should have a hydration property', function() {
+      expect(userRepository).to.have.property('activity');
     });
   });
 });
