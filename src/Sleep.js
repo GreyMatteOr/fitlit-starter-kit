e5d3cb4ae21b1117b5c8404250439a99a389fff9
Matthew Lane <themplane@gmail.com>
2020-08-25 14:21:02 -0700

    Refactor to hold the entirety of the Sleep Data.

--- src/Sleep.js
+++ src/Sleep.js
@@ -1,59 +1,79 @@
 class Sleep {
   constructor(sleepUserData) {
-    this.data = sleepUserData
+
+    // current assumptions:
+    // Only one users sleep dataOnData//
+    // It's sorted chronologically by day with no gaps
+    this.data = sleepUserData;
+
+    // new implementation:
+    // It's all users
+    // Sorted by user, then chronologically by day with no gaps
   };
 
-  calculateAverage() {
-    return this.calculateDetailAverage('hoursSlept');
+  getUserData(id) {
+    let userData = this.data.filter((sleepData) => sleepData.userID === id);
+    return userData;
+  }
+  calculateAverage(id) {
+    let userData = this.getUserData(id);
+    return this.calculateDetailAverage('hoursSlept', userData);
   }
 
-  calculateAverageQuality() {
-    return this.calculateDetailAverage('sleepQuality');
+  calculateAverageQuality(id) {
+    let userData = this.getUserData(id);
+    return this.calculateDetailAverage('sleepQuality', userData);
   }
 
-  calculateDetailAverage(detail) {
-    if(this.data.length === 0){
+  calculateDetailAverage(detail, userData) {
+    if(userData.length === 0){
       return 0;
     }
-    let total = this.data.reduce((sum, day) => {
+    let total = userData.reduce((sum, day) => {
       sum += day[detail];
       return sum;
     }, 0);
-    return total / this.data.length;
+    return total / userData.length;
   }
 
-  getHoursSleptOnDate(date) {
-    return this.getDetailOnDate('hoursSlept', date);
+  getHoursSleptOnDate(date, id) {
+    return this.getDetailOnDate('hoursSlept', date, id);
   };
 
-  getQualityOnDate(date) {
-    return this.getDetailOnDate('sleepQuality', date);
+  getQualityOnDate(date, id) {
+    return this.getDetailOnDate('sleepQuality', date, id);
   };
 
-  getDetailOnDate(detail, date) {
+  getDetailOnDate(detail, date, id) {
     let day = this.data.find((testDay) => {
-      return date === testDay.date;
+      return (date === testDay.date && id === testDay.userID);
     });
     return (day === undefined ? 0 : day[detail]);
   }
 
-  getWeeklySleepQuantity(date) {
-    return this.getDetailOnWeek('getHoursSleptOnDate', date);
+  getWeeklySleepQuantity(date, id) {
+    return this.getDetailOnWeek('hoursSlept', date, id);
   }
 
-  getWeeklySleepQuality(date) {
-    return this.getDetailOnWeek('getQualityOnDate', date);
+  getWeeklySleepQuality(date, id) {
+    return this.getDetailOnWeek('sleepQuality', date, id);
   }
 
-  getDetailOnWeek(detailFunction, date) {
-    let endOfWeek = this.data.find((day) => {
+  getDetailOnWeek(detail, date, id) {
+    let userData = this.getUserData(id);
+    userData.sort((a, b) => {
+      let [aYear, aMonth, aDay] = a.date.split('/');
+      let [bYear, bMonth, bDay] = b.date.split('/');
+      return aYear - bYear || aMonth - bMonth || aDay - bDay;
+    });
+    let endOfWeek = userData.find((day) => {
       return date === day.date;
     });
     if (endOfWeek === undefined) return [0, 0, 0, 0, 0, 0, 0];
-    let endOfWeekIndex = this.data.indexOf(endOfWeek) + 1;
+    let endOfWeekIndex = userData.indexOf(endOfWeek) + 1;
     let beginningOfWeekIndex = endOfWeekIndex - 7;
-    let week = this.data.slice(beginningOfWeekIndex, endOfWeekIndex);
-    return week.map(day => this[detailFunction](day.date));
+    let week = userData.slice(beginningOfWeekIndex, endOfWeekIndex);
+    return week.map(day => this.getDetailOnDate(detail , day.date, id));
   }
 };
 
--- test/Sleep-test.js
+++ test/Sleep-test.js
@@ -15,40 +15,82 @@ describe('Sleep', function() {
         "sleepQuality": 2.2
       },
       {
-        "userID": 1,
-        "date": "2019/06/16",
+        "userID": 2,
+        "date": "2019/06/15",
         "hoursSlept": 7,
         "sleepQuality": 4.7
       },
       {
         "userID": 1,
-        "date": "2019/06/17",
+        "date": "2019/06/16",
         "hoursSlept": 10.8,
         "sleepQuality": 4.7
       },
       {
-        "userID": 1,
-        "date": "2019/06/18",
+        "userID": 2,
+        "date": "2019/06/16",
         "hoursSlept": 5.4,
         "sleepQuality": 3
       },
       {
         "userID": 1,
-        "date": "2019/06/19",
+        "date": "2019/06/17",
         "hoursSlept": 4.1,
         "sleepQuality": 3.6
       },
       {
-        "userID": 1,
-        "date": "2019/06/20",
+        "userID": 2,
+        "date": "2019/06/17",
         "hoursSlept": 9.6,
         "sleepQuality": 2.9
       },
       {
         "userID": 1,
-        "date": "2019/06/21",
+        "date": "2019/06/18",
         "hoursSlept": 5.1,
         "sleepQuality": 2.6
+      },
+      {
+        "userID": 2,
+        "date": "2019/06/18",
+        "hoursSlept": 9.7,
+        "sleepQuality": 2.5
+      },
+      {
+        "userID": 1,
+        "date": "2019/06/19",
+        "hoursSlept": 4.8,
+        "sleepQuality": 4.9
+      },
+      {
+        "userID": 2,
+        "date": "2019/06/19",
+        "hoursSlept": 5.4,
+        "sleepQuality": 2.4
+      },
+      {
+        "userID": 1,
+        "date": "2019/06/20",
+        "hoursSlept": 5.4,
+        "sleepQuality": 3.9
+      },
+      {
+        "userID": 2,
+        "date": "2019/06/20",
+        "hoursSlept": 5.5,
+        "sleepQuality": 4.6
+      },
+      {
+        "userID": 1,
+        "date": "2019/06/21",
+        "hoursSlept": 8.7,
+        "sleepQuality": 1.9
+      },
+      {
+        "userID": 2,
+        "date": "2019/06/21",
+        "hoursSlept": 9.2,
+        "sleepQuality": 4.6
       }
     ];
     sleep = new Sleep(sleepUserData);
@@ -68,45 +110,95 @@ describe('Sleep', function() {
     });
   });
 
+  describe('getUserData()', function() {
+    it(`should create an Array of only one User's data`, function() {
+      let user1Data = [
+        {
+          "userID": 1,
+          "date": "2019/06/15",
+          "hoursSlept": 6.1,
+          "sleepQuality": 2.2
+        },
+        {
+          "userID": 1,
+          "date": "2019/06/16",
+          "hoursSlept": 10.8,
+          "sleepQuality": 4.7
+        },
+        {
+          "userID": 1,
+          "date": "2019/06/17",
+          "hoursSlept": 4.1,
+          "sleepQuality": 3.6
+        },
+        {
+          "userID": 1,
+          "date": "2019/06/18",
+          "hoursSlept": 5.1,
+          "sleepQuality": 2.6
+        },
+        {
+          "userID": 1,
+          "date": "2019/06/19",
+          "hoursSlept": 4.8,
+          "sleepQuality": 4.9
+        },
+        {
+          "userID": 1,
+          "date": "2019/06/20",
+          "hoursSlept": 5.4,
+          "sleepQuality": 3.9
+        },
+        {
+          "userID": 1,
+          "date": "2019/06/21",
+          "hoursSlept": 8.7,
+          "sleepQuality": 1.9
+        }
+      ]
+      expect(sleep.getUserData(1)).to.deep.equal(user1Data);
+    });
+  });
+
   describe('calculateAverage()', function() {
     it('should calculate the average number of hours a user has slept per day', function() {
-      expect(sleep.calculateAverage()).to.equal(48.1 / 7);
+      expect(sleep.calculateAverage(1)).to.equal(45 / 7);
     });
 
     it('should return 0 if there is no sleep data available', function() {
       sleep = new Sleep([]);
-      expect(sleep.calculateAverage()).to.equal(0);
+      expect(sleep.calculateAverage(1)).to.equal(0);
     });
   });
 
   describe('calculateAverageQuality()', function() {
     it('should calculate the average sleep quality per day over all time', function() {
-      expect(sleep.calculateAverageQuality()).to.be.closeTo((23.7 / 7), .0001);
+      expect(sleep.calculateAverageQuality(1)).to.be.closeTo((23.8 / 7), .0001);
     });
 
     it('should return 0 if there is no sleep data available', function() {
       sleep = new Sleep([]);
-      expect(sleep.calculateAverageQuality()).to.equal(0);
+      expect(sleep.calculateAverageQuality(1)).to.equal(0);
     });
   });
 
   describe('getHoursSleptOnDate()', function() {
     it('should return the amount of hours slept for a specific date', function() {
-      expect(sleep.getHoursSleptOnDate("2019/06/17")).to.equal(10.8);
+      expect(sleep.getHoursSleptOnDate("2019/06/17", 1)).to.equal(4.1);
     });
 
     it('should return 0 if there is no sleep data available', function() {
-      expect(sleep.getHoursSleptOnDate("2189/06/17")).to.equal(0);
+      expect(sleep.getHoursSleptOnDate("2189/06/17", 1)).to.equal(0);
     });
   });
 
   describe('getQualityOnDate()', function() {
     it('should return the quality of sleep for a specific date', function() {
-      expect(sleep.getQualityOnDate("2019/06/17")).to.equal(4.7);
+      expect(sleep.getQualityOnDate("2019/06/17", 1)).to.equal(3.6);
     });
 
     it('should return 0 if there is no sleep data available', function() {
-      expect(sleep.getQualityOnDate("2189/06/17")).to.closeTo((0), .0001);
+      expect(sleep.getQualityOnDate("2189/06/17", 1)).to.closeTo((0), .0001);
     });
   });
 
@@ -114,14 +206,14 @@ describe('Sleep', function() {
     it('should return an Array of hoursSlept for 7 days ending on the date passed in', function() {
       let weekOfSleepData = [
         6.1,
-        7,
         10.8,
-        5.4,
         4.1,
-        9.6,
-        5.1
+        5.1,
+        4.8,
+        5.4,
+        8.7
       ];
-      expect(sleep.getWeeklySleepQuantity("2019/06/21")).to.deep.equal(weekOfSleepData);
+      expect(sleep.getWeeklySleepQuantity("2019/06/21", 1)).to.deep.equal(weekOfSleepData);
     });
 
     it('should return an array of seven `0`-s if the end date is not defined', function () {
@@ -134,13 +226,13 @@ describe('Sleep', function() {
       let weekOfSleepData = [
         2.2,
         4.7,
-        4.7,
-        3,
         3.6,
-        2.9,
-        2.6
+        2.6,
+        4.9,
+        3.9,
+        1.9
       ];
-      expect(sleep.getWeeklySleepQuality("2019/06/21")).to.deep.equal(weekOfSleepData);
+      expect(sleep.getWeeklySleepQuality("2019/06/21", 1)).to.deep.equal(weekOfSleepData);
     });
 
     it('should return an array of seven `0`-s if the end date is not defined', function () {
