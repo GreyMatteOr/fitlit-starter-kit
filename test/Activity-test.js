a9069697997f8ab10d29081098a1f1f03ba44839
Matthew Lane <themplane@gmail.com>
2020-08-26 14:19:25 -0700

    Initialize Activity Class. Add Tests

--- test/Activity-test.js
+++ test/Activity-test.js
@@ -5,9 +5,110 @@ const Activity = require('../src/Activity');
 
 describe('Activity', function() {
 
-  let activity;
+  let activity, userData;
   beforeEach(function() {
-    activity = new Activity();
+    userData = [
+        {
+        "userID": 1,
+        "date": "2019/06/15",
+        "numSteps": 3577,
+        "minutesActive": 140,
+        "flightsOfStairs": 16
+      },
+      {
+        "userID": 2,
+        "date": "2019/06/15",
+        "numSteps": 4294,
+        "minutesActive": 138,
+        "flightsOfStairs": 10
+      },
+      {
+        "userID": 1,
+        "date": "2019/06/16",
+        "numSteps": 7402,
+        "minutesActive": 116,
+        "flightsOfStairs": 33
+      },
+      {
+        "userID": 2,
+        "date": "2019/06/16",
+        "numSteps": 3486,
+        "minutesActive": 114,
+        "flightsOfStairs": 32
+      },
+      {
+        "userID": 1,
+        "date": "2019/06/17",
+        "numSteps": 11374,
+        "minutesActive": 213,
+        "flightsOfStairs": 13
+      },
+      {
+        "userID": 2,
+        "date": "2019/06/17",
+        "numSteps": 14810,
+        "minutesActive": 287,
+        "flightsOfStairs": 18
+      },
+      {
+        "userID": 1,
+        "date": "2019/06/18",
+        "numSteps": 2634,
+        "minutesActive": 107,
+        "flightsOfStairs": 5
+      },
+      {
+        "userID": 2,
+        "date": "2019/06/18",
+        "numSteps": 10333,
+        "minutesActive": 114,
+        "flightsOfStairs": 31
+      },
+      {
+        "userID": 1,
+        "date": "2019/06/19",
+        "numSteps": 6389,
+        "minutesActive": 41,
+        "flightsOfStairs": 33
+      },
+      {
+        "userID": 2,
+        "date": "2019/06/19",
+        "numSteps": 8015,
+        "minutesActive": 106,
+        "flightsOfStairs": 37
+      },
+      {
+        "userID": 1,
+        "date": "2019/06/20",
+        "numSteps": 11652,
+        "minutesActive": 20,
+        "flightsOfStairs": 24
+      },
+      {
+        "userID": 2,
+        "date": "2019/06/20",
+        "numSteps": 9256,
+        "minutesActive": 108,
+        "flightsOfStairs": 2
+      },
+      {
+        "userID": 1,
+        "date": "2019/06/21",
+        "numSteps": 9303,
+        "minutesActive": 27,
+        "flightsOfStairs": 14
+      },
+      {
+        "userID": 2,
+        "date": "2019/06/21",
+        "numSteps": 8024,
+        "minutesActive": 216,
+        "flightsOfStairs": 32
+      }
+    ];
+    dateEnd = '2019/06/21';
+    activity = new Activity(userData);
   });
 
   describe('Intialization', function() {
@@ -15,8 +116,13 @@ describe('Activity', function() {
     it('should be a function', function() {
       expect(Activity).to.be.a('function');
     });
+
     it('should be an instance of Activity', function() {
       expect(activity).to.be.an.instanceof(Activity);
+    });
+
+    it('should hold userData', function() {
+      expect(activity.data).to.deep.equal(userData);
     });
   });
 });
