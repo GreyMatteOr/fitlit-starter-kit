9029cb4f1c69cabab9f3e6cbf09a8a97f536005f
Eric Berglund <ericberglund117@gmail.com>
2020-08-26 13:48:10 -0600

    Display user sleep data for latest day

--- src/Sleep.js
+++ src/Sleep.js
@@ -140,4 +140,6 @@ class Sleep {
   }
 };
 
-module.exports = Sleep;
+if (typeof module !== 'undefined') {
+  module.exports = Sleep;
+}
--- src/scripts.js
+++ src/scripts.js
@@ -1,12 +1,14 @@
 let greeting = document.querySelector('h1');
 const users = new UserRepository(userData);
 const hydration = new Hydration(hydrationData);
+const sleep = new Sleep(sleepData);
 window.onload = onLoad
 
 function onLoad () {
   let randomUser = getRandomUser();
   displayStepGoalMessage(randomUser);
   displayHydration(randomUser);
+  displaySleep(randomUser);
 }
 
 function getRandomUser() {
@@ -21,7 +23,7 @@ function displayStepGoalMessage(user) {
 }
 
 function displayHydration(user) {
-  let lastDay = '2019/09/22'
+  let lastDay = '2019/09/22';
   let dailyHydration = hydration.findOuncesWaterOfDay(lastDay, user.id);
   let averageHydrationOverLatestWeek = hydration.findOuncesWaterOfWeekBefore(lastDay, user.id);
   averageHydrationOverLatestWeek.pop();
@@ -32,3 +34,11 @@ function displayHydration(user) {
     Your hydration for today is ${dailyHydration}.` + message;
   greeting.innerText += message;
 };
+
+function displaySleep(user) {
+  let lastDay = '2019/09/22';
+  let recentSleepHours = sleep.getHoursSleptOnDate(lastDay, user.id);
+  let recentSleepQuality = sleep.getQualityOnDate(lastDay, user.id);
+  let adjective = (recentSleepQuality >= 4 ? 'very deeply' : (recentSleepQuality >= 3 ? 'deeply' : (recentSleepQuality >= 2 ? 'alright' : 'poorly')));
+  greeting.innerText += `\n${user.getFirstName()} slept ${adjective} for ${recentSleepHours} hours`
+}
